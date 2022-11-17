0cc6ae34f44047ab3c3cdb53c9da4eaed35b43c4
AntonioFarias <antoniotercerio@gmail.com>
2022-11-16 21:12:16 -0300

    fix : init

--- getCityWeather/__init__.py
+++ getCityWeather/__init__.py
@@ -2,8 +2,7 @@ import logging
 import azure.functions as func
 import requests 
 from app.generateID import GenerateID
-import datetime as dt
-import time
+from functions.cleanRequest import clean_request
 
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = 'd85eaab2aca141c7138eff86c67a98a7'
@@ -11,10 +10,6 @@ API_KEY = 'd85eaab2aca141c7138eff86c67a98a7'
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('The function processed a request.')
 
-
-    """ user_test = GenerateID(name)
-    user_test = user_test.associate_city(city) """
-  
     req_body = req.get_json()
     
     city = req_body.get('city')
@@ -24,7 +19,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if city:
         response = requests.get(url).json()
-        return func.HttpResponse(f"Hello {name},this function executed successfully. We are collecting the data about {city} \n Here is the information: {response}")   
+
+        weather = clean_request(response)
+
+        user = GenerateID(name)
+
+        user.associate_city(city)
+        
+        return func.HttpResponse(f"Hello {user.username},this function executed successfully. We are collecting the data about {user.city} \n This is the temperature in {user.city} {weather:.1f}°C")   
     else:
         return func.HttpResponse(
              "This function executed successfully. Pass a city in the query string or in the request body for info about yout city.",

1bb70814a877805d2405ff86de1a05f4617996ae
AntonioFarias <antoniotercerio@gmail.com>
2022-11-16 21:13:13 -0300

    fix: object-name

--- tests/test_generateID.py
+++ tests/test_generateID.py
@@ -1,10 +1,11 @@
-import unittest
 from app.generateID import GenerateID
+import unittest
+
 
 class TestGenerateId(unittest.TestCase):
     def test_associate_city_is_string(self):
         self.assertIsInstance(GenerateID.associate_city('Antonio'),str)
     
     def test_is_int_random_id(self):
-        self.assertIsInstance(self.__id,int)
+        self.assertIsInstance(self._id,int)
     

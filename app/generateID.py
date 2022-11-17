b1afb4d7c746fbee2196d82d2d3024d32839def0
AntonioFarias <antoniotercerio@gmail.com>
2022-11-16 21:11:46 -0300

    fix: constructor-attribute

--- app/generateID.py
+++ app/generateID.py
@@ -2,24 +2,23 @@ from random import randint
 
 
 class GenerateID:
-    def __init__(self,username:str) -> None:
-        self.username = username
-        self.__id = randint(00000,99999)
+    def __init__(self,username) -> None:
+        self._username = username
+        self._id = randint(00000,99999)
         self._city = ''
 
     @property
     def username(self):
-        return self.username
+        return self._username
     
     @property
     def id(self):
-        return self.__id
+        return self._id
     
-   
-    @id.setter
-    def id(self,new_id):
-        self.__id = new_id
-        
+    @property
+    def city(self):
+        return self._city
+
     def associate_city(self,city):
         self._city = city
 
